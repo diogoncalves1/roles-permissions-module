9898189171afe087a14aa41ec060682cb3c6e384
diogogoncalves <dmgdark95@gmail.com>
2025-07-18 13:25:55 +0100

    Upload

---
+++ routes/api.php
@@ -0,0 +1,4 @@
+<?php
+
+use App\Http\Controllers\RoleController;
+use Illuminate\Support\Facades\Route;
---
+++ routes/web.php
@@ -0,0 +1,4 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
