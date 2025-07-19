3b5885763b7c9406931ee054c61c90fe7ef2c9dd
diogogoncalves <dmgdark95@gmail.com>
2025-07-19 21:49:27 +0100

    Routes Web

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,22 @@
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
+
+Route::group([
+    "prefix" => "admin",
+    "as" => "admin.",
+    "middleware" => ["auth"]
+], function () {
+    Route::group(
+        [
+            "prefix" => "roles",
+            "as" => "roles."
+        ],
+        function () {
+            Route::get("manage/{id}", [\App\Http\Controllers\RoleController::class, "showManageForm"]);
+            Route::post("manage{id}", [\App\Http\Controllers\RoleController::class, "manage"]);
+        }
+    );
+    Route::resource("roles", \App\Http\Controllers\RoleController::class, ['except' => 'show']);
+    Route::resource("permissions", \App\Http\Controllers\PermissionsController::class, ['except' => 'show']);
+});
