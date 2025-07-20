44f15db49706a70247fc5cc46170fa375fdc7753
diogogoncalves <dmgdark95@gmail.com>
2025-07-21 00:45:50 +0100

    Repositories

--- app/Repositories/RepositoryInterface.php
+++ app/Repositories/RepositoryInterface.php
@@ -2,31 +2,14 @@
 
 namespace App\Repositories;
 
-use App\Http\Requests\PermissionRequest;
-use App\Models\Permissions;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class RepositoryInterface
 {
-    public function all()
-    {
-        Permissions::all();
-    }
+    public function all() {}
 
-    public function store(PermissionRequest $request)
-    {
-        try {
-            $input = $request->all();
-            DB::transaction(function () use ($input) {
-                Permissions::create($input);
-            });
-        } catch (\Exception $e) {
-            Log::error($e);
-        }
-    }
+    public function store() {}
 
-    public function update(PermissionRequest $request, string $id) {}
+    public function update() {}
 
     public function destroy(string $id) {}
 
